34ddd45a9711762c0a08921a3999bdd1ef3c0728
ITO <ITO@SalITOC0061.focusservices.com>
2023-11-14 14:18:45 -0600

    Added middleware to validate token & protect private routes

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,18 +1,20 @@
-import express from "express"
-import morgan from "morgan"
-import cors from 'cors'
-import authRoutes from './routes/auth.routes.js'
+import express from "express";
+import morgan from "morgan";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import authRoutes from "./routes/auth.routes.js";
 
-const app = express()
+const app = express();
 
 // Settings
-app.set('port', 4000)
+app.set("port", 4000);
 
 // Middlewares
-app.use(cors())
-app.use(morgan('dev'))
-app.use(express.json())
+app.use(cors());
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(cookieParser())
 
-app.use("/api", authRoutes)
+app.use("/api", authRoutes);
 
-export default app
+export default app;
--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -8,7 +8,7 @@ export const register = async (req, res) => {
   try {
     //Hash password
     const hashedPassword = await bcrypt.hash(password, 10);
-    
+
     //Create new user object
     const newUser = new User({
       username,
@@ -19,22 +19,22 @@ export const register = async (req, res) => {
 
     //Save new user in db
     const userSaved = await newUser.save();
-    
+
     //Create auth token
-    const token = await createAccessToken({id: userSaved._id})
-    
+    const token = await createAccessToken({ id: userSaved._id });
+
     //Save token in cookies
-    res.cookie('token', token)
-    
+    res.cookie("token", token);
+
     //Return the credentials to the user
     res.json({
       id: userSaved._id,
       username: userSaved.username,
       email: userSaved.email,
-      type: userSaved.type
-    })
+      type: userSaved.type,
+    });
   } catch (error) {
-    res.status(500).json({message: error.message})
+    res.status(500).json({ message: error.message });
   }
 };
 export const login = async (req, res) => {
@@ -43,35 +43,50 @@ export const login = async (req, res) => {
 
   try {
     //Find if the user exist
-    const userFound = await User.findOne({email})
-    if(!userFound) return res.status(400).json({message: "Invalid credentials"})
+    const userFound = await User.findOne({ email });
+    if (!userFound)
+      return res.status(400).json({ message: "Invalid credentials" });
 
     //Check password
     const isPasswordMatch = await bcrypt.compare(password, userFound.password);
 
-    if(!isPasswordMatch) return res.status(400).json({message: "Invalid credentials"})
-    
+    if (!isPasswordMatch)
+      return res.status(400).json({ message: "Invalid credentials" });
+
     //Create auth token
-    const token = await createAccessToken({id: userFound._id})
-    
+    const token = await createAccessToken({ id: userFound._id });
+
     //Save token in cookies
-    res.cookie('token', token)
-    
+    res.cookie("token", token);
+
     //Return the credentials to the user
     res.json({
       id: userFound._id,
       username: userFound.username,
       email: userFound.email,
-      type: userFound.type
-    })
+      type: userFound.type,
+    });
   } catch (error) {
-    res.status(500).json({message: error.message})
+    res.status(500).json({ message: error.message });
   }
 };
 
 export const logout = (req, res) => {
-  res.cookie('token', "", {
-    expires: new Date(0)
+  res.cookie("token", "", {
+    expires: new Date(0),
+  });
+  return res.sendStatus(200);
+};
+
+export const profile = async (req, res) => {
+  const userFound = await User.findById(req.user.id)
+  if(!userFound) return res.status(400).json({message: "User not found"})
+
+  return res.json({
+    id: userFound._id,
+    username: userFound.username,
+    email: userFound.email,
+    type: userFound.type
   })
-  return res.sendStatus(200)
-}
+  res.send("profile");
+};
