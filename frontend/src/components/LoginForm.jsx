91948f4898dac84e189e099972567a482cd41036
ITO <ITO@SalITOC0061.focusservices.com>
2023-11-15 13:35:01 -0600

    Added form structure to login

---
+++ frontend/src/components/LoginForm.jsx
@@ -0,0 +1,75 @@
+import React from "react";
+import { useForm } from "react-hook-form";
+
+const LoginForm = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = handleSubmit((data, errors) => {
+    console.log("data: ", data);
+  });
+
+  return (
+    <div className="card">
+      <div className="card-body">
+        <h3 className="card-title">Login</h3>
+        <form onSubmit={onSubmit}>
+          <div className="form-group">
+            <div>
+              <label htmlFor="exampleInputEmail1">Email address</label>
+              <input
+                type="email"
+                className="form-control"
+                name="email"
+                id="exampleInputEmail1"
+                aria-describedby="emailHelp"
+                placeholder="Enter email"
+                {...register("email", {
+                  required: "Email Address is required",
+                })}
+              />
+            </div>
+            {errors.email ? (
+              <>
+                {errors.email.type === "required" && (
+                  <span>{errors.email.message}</span>
+                )}
+              </>
+            ) : null}
+          </div>
+
+          <div className="form-group">
+            <div>
+              <label htmlFor="exampleInputPassword1">Password</label>
+              <input
+                type="password"
+                name="password"
+                className="form-control"
+                id="exampleInputPassword1"
+                placeholder="Password"
+                {...register("password", {
+                  required: "Password is required",
+                })}
+              />
+            </div>
+            {errors.password ? (
+              <>
+                {errors.password.type === "required" && (
+                  <span>{errors.password.message}</span>
+                )}
+              </>
+            ) : null}
+          </div>
+          <button type="submit" className="btn btn-primary">
+            Submit
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default LoginForm;
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import LoginForm from '../components/LoginForm'
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <div className='container h-100 w-50 mt-5'>
+      <h1 className='text-center pt-5 mb-5'>Welcome to My U Library</h1>
+      <LoginForm/>
+    </div>
   )
 }
 
