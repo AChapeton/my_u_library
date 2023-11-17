ab6fb5f0276f342b033e80aaedef58b33be02b37
AChapeton <andres.chapeton0206@gmail.com>
2023-11-17 10:15:27 -0600

    ip added

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,7 +3,7 @@ import { connectDB } from "./database.js"
 
 const main = async () =>{
   await connectDB()
-  await app.listen(app.get('port'))
+  await app.listen(app.get('port'), "0.0.0.0")
   console.log('Server on port ', process.env.PORT)
 }
 
