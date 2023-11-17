50d12f779346427a0c6db372c666e0506e3a2c22
AChapeton <andres.chapeton0206@gmail.com>
2023-11-17 07:30:51 -0600

    Changed config

--- backend/src/libs/jwt.js
+++ backend/src/libs/jwt.js
@@ -1,5 +1,5 @@
 import jwt from 'jsonwebtoken'
-import { TOKEN_SECRET } from '../../config';
+import { TOKEN_SECRET } from '../config';
 
 export const createAccessToken = (payload) => {
   return new Promise((resolve, reject) => {
